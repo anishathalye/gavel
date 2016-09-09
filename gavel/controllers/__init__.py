5a323d900ed353f11d45d29526152ba5956dc7fd
Anish Athalye <me@anishathalye.com>
2016-09-09 12:51:03 -0400

    Add favicon

--- gavel/controllers/__init__.py
+++ gavel/controllers/__init__.py
@@ -1,3 +1,4 @@
+import gavel.controllers.default
 import gavel.controllers.csrf_protection
 import gavel.controllers.admin
 import gavel.controllers.api
---
+++ gavel/controllers/default.py
@@ -0,0 +1,11 @@
+from gavel import app
+from flask import send_from_directory
+import os
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
