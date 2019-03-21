c678d65b9b94a8571dab3cc191dba18dcfc51252
Muhammad Khatri <khizar3840@gmail.com>
2019-03-20 19:08:31 -0500

    Judges panel auto-updates

--- gavel/static/js/admin_live.js
+++ gavel/static/js/admin_live.js
@@ -10,7 +10,6 @@ async function refresh(token) {
             return data;
         }
     });
-    console.log(data);
     const annotators = data.annotators;
     const counts = data.counts;
     const item_counts = data.item_counts;
@@ -30,7 +29,7 @@ async function refresh(token) {
     for (let i = 0; i < flags.length; i++) {
         const flag = flags[i];
         const annotator = await annotators[flag.annotator_id];
-        const project = await items[flag.project_id];
+        const item = await items[flag.project_id];
 
         if (!flag.id)
             continue;
@@ -41,11 +40,12 @@ async function refresh(token) {
         const reports_template = '<tr class="' + resolved + '">\n' +
             '              <td><span class="admin-check"></span></td>\n' +
             '              <td>' + flag.id + '</td>\n' +
-            '              <td>' + annotator.name + '</td>\n' +
-            '              <td>' + project.name + '</td>\n' +
-            '              <td>' + project.location + '</td>\n' +
+            '              <td><a href="/admin/annotator/'+annotator.id+'"\n' +
+            '                     class="colored">'+annotator.name+'</a></td>\n' +
+            '              <td><a href="/admin/item/'+item.id+'" class="colored">' + item.name + '</a></td>\n' +
+            '              <td>' + item.location + '</td>\n' +
             '              <td>' + flag.reason + '</td>\n' +
-            '              <td class="compact" data-sort="{{ not flag.resolved }}">\n' +
+            '              <td class="compact">\n' +
             '                <form action="/admin/report" method="post">\n' +
             '                  <input type="submit" name="action" value="' + resolved3 + '"\n' +
             '                         class="' + resolved2 + '">\n' +
@@ -61,42 +61,6 @@ async function refresh(token) {
 
     }
 
-    /*<tr class="{{ 'disabled' if not item.active else 'prioritized' if item.prioritized else ''}}">
-              <td><span class="admin-check"></span></td>
-              <td><a href="{{ url_for('item_detail', item_id=item.id) }}" class="colored">{{ item.id }}</a></td>
-              <td>{{ item.name | safe }}</td>
-              <td>{{ item.location | safe }}</td>
-              <td class="preserve-formatting">{{ item.description | safe }}</td>
-              <td>{{ item.mu | round(4) }}</td>
-              <td>{{ item.sigma_sq | round(4) }}</td>
-              <td>{{ item_counts.get(item.id, 0) }}</td>
-              <td>{{ item.viewed | length }}</td>
-              <td>{{ skipped.get(item.id, 0) }}</td>
-              <td class="compact" data-sort="{{ item.prioritized }}">
-                <form action="/admin/item" method="post">
-                  <input type="submit" name="action" value="{{ 'Prioritize' if not item.prioritized else 'Cancel' }}"
-                         class="{{ 'positive' if not item.prioritized else 'negative' }}">
-                  <input type="hidden" name="item_id" value="{{ item.id }}">
-                  <input type="hidden" name="_csrf_token" value="{{ csrf_token() }}">
-                </form>
-              </td>
-              <td class="compact" data-sort="{{ item.active }}">
-                <form action="/admin/item" method="post">
-                  <input type="submit" name="action" value="{{ 'Disable' if item.active else 'Enable' }}"
-                         class="{{ 'negative' if item.active else 'positive' }}">
-                  <input type="hidden" name="item_id" value="{{ item.id }}">
-                  <input type="hidden" name="_csrf_token" value="{{ csrf_token() }}">
-                </form>
-              </td>
-              <td class="compact">
-                <form action="/admin/item" method="post">
-                  <input type="submit" name="action" value="Delete" class="negative">
-                  <input type="hidden" name="item_id" value="{{ item.id }}">
-                  <input type="hidden" name="_csrf_token" value="{{ csrf_token() }}">
-                </form>
-              </td>
-            </tr>*/
-
     // Populate projects
     let projects_table = document.getElementById("items-body");
     projects_table.innerHTML = "";
@@ -109,7 +73,7 @@ async function refresh(token) {
 
         const items_template = '<tr class="' + (item.active ? item.prioritized ? 'prioritized' : '' : 'disabled') + '">\n' +
             '              <td><span class="admin-check"></span></td>\n' +
-            '              <td><a href="{{ url_for(\'item_detail\', item_id=' + item.id + ') }}" class="colored">' + item.id + '</a></td>\n' +
+            '              <td><a href="/admin/item/'+item.id+'" class="colored">' + item.id + '</a></td>\n' +
             '              <td>' + item.name + '</td>\n' +
             '              <td>' + item.location + '</td>\n' +
             '              <td class="preserve-formatting">' + item.description + '</td>\n' +
@@ -143,11 +107,58 @@ async function refresh(token) {
             '              </td>\n' +
             '            </tr>';
 
-
-        //TODO: Can use item.placement object
         const newRow = projects_table.insertRow(projects_table.rows.length);
         newRow.innerHTML = items_template;
 
+    }
+
+    // Populate Judges
+    let judges_table = document.getElementById("judges-body");
+    judges_table.innerHTML = "";
+    for (let i = 0; i < annotators.length; i++) {
+        const annotator = annotators[i];
+
+        if (!annotator.id)
+            continue;
+
+        const annotator_template = '<tr class="' + (annotator.active ? '' : 'disabled') + '">\n' +
+            '              <td><span class="admin-check"></span></td>\n' +
+            '              <td><a href="/admin/annotator/'+annotator.id+'"\n' +
+            '                     class="colored">'+annotator.id+'</a></td>\n' +
+            '              <td>' + annotator.name + '</td>\n' +
+            '              <td>' + annotator.email + '</td>\n' +
+            '              <td>' + annotator.description + '</td>\n' +
+            '              <td>' + (counts[annotator.id]||0) + '</td>\n' +
+            '              <td data-sort="{{ annotator.next_id or -1 }}">' + (annotator.next_id||'None') + '</td>\n' +
+            '              <td data-sort="{{ annotator.prev_id or -1 }}">' + (annotator.prev_id||'None') + '</td>\n' +
+            '              <td\n' +
+            '                data-sort="{{ annotator.updated | utcdatetime_epoch }}">' + (annotator.updated || 'Undefined') + '</td>\n' +
+            '              <td class="compact">\n' +
+            '                <form action="/admin/annotator" method="post">\n' +
+            '                  <input type="submit" name="action" value="Email" class="neutral">\n' +
+            '                  <input type="hidden" name="annotator_id" value="' + annotator.id + '">\n' +
+            '                  <input type="hidden" name="_csrf_token" value="' + token + '">\n' +
+            '                </form>\n' +
+            '              </td>\n' +
+            '              <td class="compact" data-sort="' + annotator.active + '">\n' +
+            '                <form action="/admin/annotator" method="post">\n' +
+            '                  <input type="submit" name="action" value="' + (annotator.active ? 'Disable' : 'Enable') + '"\n' +
+            '                         class="' + (annotator.active ? 'negative' : 'positive') + '">\n' +
+            '                  <input type="hidden" name="annotator_id" value="' + annotator.id + '">\n' +
+            '                  <input type="hidden" name="_csrf_token" value="' + token + '">\n' +
+            '                </form>\n' +
+            '              </td>\n' +
+            '              <td class="compact">\n' +
+            '                <form action="/admin/annotator" method="post">\n' +
+            '                  <input type="submit" name="action" value="Delete" class="negative">\n' +
+            '                  <input type="hidden" name="annotator_id" value="' + annotator.id + '">\n' +
+            '                  <input type="hidden" name="_csrf_token" value="' + token + '">\n' +
+            '                </form>\n' +
+            '              </td>\n' +
+            '            </tr>';
+
+        const newRow = judges_table.insertRow(judges_table.rows.length);
+        newRow.innerHTML = annotator_template;
     }
 
     $('#judges').trigger("update");
