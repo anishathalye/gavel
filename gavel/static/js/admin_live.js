cc7c876c01deb8b72cc432db31ee5b92176ed82c
Muhammad Khatri <khizar3840@gmail.com>
2019-04-24 21:13:41 -0500

    Checking top checkbox checks all

--- gavel/static/js/admin_live.js
+++ gavel/static/js/admin_live.js
@@ -46,28 +46,25 @@ async function refresh(token) {
     let reports_table = document.getElementById("reports-body");
     reports_table.innerHTML = "";
     for (let i = 0; i < flags.length; i++) {
-        const flag = flags[i];
-        const annotator = await annotators[flag.annotator_id];
-        const item = await items[flag.project_id];
-
-        if (!flag.id)
-            continue;
 
         try {
+            const flag = flags[i];
+            const annotator = await annotators[flag.annotator_id];
+            const item = await items[flag.project_id];
 
-        } catch (e) {
+            if (!flag.id)
+                continue;
             const reports_template = `
             <tr class="${flag.resolved ? "open" : "resolve"}">
-              <td><span class="admin-check"></span></td>
-              <td>'${flag.id}'</td>
-              <td><a href="/admin/annotator/${annotator.id}"
-                     class="colored">${annotator.name}</a></td>
-              <td><a href="/admin/item/${item.id}" class="colored">${item.name}</a></td>
+              <td><input id="${flag.id}" type="checkbox" name="item" value="${item.id}" class="admin-check"/></td>
+              <td>${flag.id}</td>
+              <td><a onclick="openJudge(${annotator.id})" href="#" class="colored">${annotator.name}</a></td>
+              <td><a onclick="openProject(${item.id})" href="#" class="colored">${item.name}</a></td>
               <td>${item.location}</td>
               <td>${flag.reason}</td>
           <td class="compact">
             <form action="/admin/item" method="post">
-              <input type="submit" name="action" value="${(item.active ? 'Disable' : 'Enable')}" class="${(item.active? 'negative': 'positive')}">
+              <input type="submit" name="action" value="${(item.active ? 'Disable' : 'Enable')}" class="${(item.active ? 'negative' : 'positive')}">
               <input type="hidden" name="item_id" value="${item.id}">
               <input type="hidden" name="_csrf_token" value="${token}">
             </form>
@@ -83,9 +80,12 @@ async function refresh(token) {
             </tr>`;
 
 
-        const newRow = reports_table.insertRow(reports_table.rows.length);
-        newRow.innerHTML = reports_template;
+            const newRow = reports_table.insertRow(reports_table.rows.length);
+            newRow.innerHTML = reports_template;
 
+
+        } catch (e) {
+            console.log(e);
         }
 
     }
@@ -93,7 +93,7 @@ async function refresh(token) {
     // Populate projects
     let projects_table = document.getElementById("items-body");
     projects_table.innerHTML = "";
-    currentItems=items;
+    currentItems = items;
     for (let i = 0; i < items.length; i++) {
         try {
             const item = items[i];
@@ -104,7 +104,7 @@ async function refresh(token) {
             // language=HTML
             const items_template = `
             <tr class="${(item.active ? item.prioritized ? 'prioritized' : '' : 'disabled')}">
-              <td><span class="admin-check"></span></td>
+              <td><input id="${item.id}" type="checkbox" name="item" value="${item.id}" class="admin-check"/></td>
               <td><a onclick="openProject(${item.id})" class="colored">${i}</a></td>
               <td>${item.name}</td>
               <td>${item.location}</td>
@@ -139,8 +139,8 @@ async function refresh(token) {
               </td>
             </tr>`;
 
-        const newRow = projects_table.insertRow(projects_table.rows.length);
-        newRow.innerHTML = items_template;
+            const newRow = projects_table.insertRow(projects_table.rows.length);
+            newRow.innerHTML = items_template;
         } catch (e) {
             console.log(e)
         }
@@ -150,7 +150,7 @@ async function refresh(token) {
     // Populate Judges
     let judges_table = document.getElementById("judges-body");
     judges_table.innerHTML = "";
-    currentAnnotators = [];
+    currentAnnotators = annotators;
 
     for (let i = 0; i < annotators.length; i++) {
         try {
@@ -161,15 +161,15 @@ async function refresh(token) {
 
             // language=HTML
             const annotator_template = `
-            <tr class=${annotator.active? '' : 'disabled'}>
-              <td><input type="checkbox" name="annotator" value="${annotator.id}" class="admin-check"/></td>
+            <tr class=${annotator.active ? '' : 'disabled'}>
+              <td><input id="${annotator.id}" type="checkbox" name="annotator" value="${annotator.id}" class="admin-check"/></td>
               <td><a onclick="openJudge(${annotator.id})" class="colored">${i}</a></td>
               <td>${annotator.name}</td>
               <td>${annotator.email}</td>
               <td>${annotator.description}</td>
-              <td>${(counts[annotator.id]||0)}</td>
-              <td>${(annotator.next_id||'None')}</td>
-              <td>${(annotator.prev_id||'None')}</td>
+              <td>${(counts[annotator.id] || 0)}</td>
+              <td>${(annotator.next_id || 'None')}</td>
+              <td>${(annotator.prev_id || 'None')}</td>
               <td>${(annotator.updated || 'Undefined')}</td>
               <td class="compact">
                 <form action="/admin/annotator" method="post">
@@ -195,8 +195,8 @@ async function refresh(token) {
               </td>
             </tr>`;
 
-        const newRow = judges_table.insertRow(judges_table.rows.length);
-        newRow.innerHTML = annotator_template;
+            const newRow = judges_table.insertRow(judges_table.rows.length);
+            newRow.innerHTML = annotator_template;
         } catch (e) {
             console.log(e);
         }
