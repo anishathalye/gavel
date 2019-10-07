7a73272fda4c77da5fbf0b9c61108d0d3cfc751c
htoo97 <htoo97@gmail.com>
2019-10-06 20:41:33 -0700

    Change date format to show in local exact time

--- gavel/template_filters.py
+++ gavel/template_filters.py
@@ -1,14 +1,20 @@
+from datetime import datetime, timezone
 from gavel import app
 from humanize import naturaltime
+
 
 @app.template_filter('utcdatetime_local')
 def _jinja2_filter_datetime_local(datetime):
     if datetime is None:
         return 'None'
-    return naturaltime(datetime)
+    return utc_to_local(datetime)
+
 
 @app.template_filter('utcdatetime_epoch')
 def _jinja2_filter_datetime_epoch(datetime):
     if datetime is None:
         return 0
     return datetime.strftime('%s')
+
+def utc_to_local(utc_dt):
+    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
