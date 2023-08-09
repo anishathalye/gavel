e12f9435c5f9b165c4f9d8d5d1ce6431e0bc5839
Shane Stevens <45518527+shanestevenz@users.noreply.github.com>
2023-08-09 15:57:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 import smtplib
 
+gmail_user = 'hackathontest.rtx@gmail.com'
+gmail_password = 'Raytheon123!'
+
+sent_from = gmail_user
+to = ['shane.stevens@rtx.com']
+subject = 'Test Email From Heroku'
+body = 'This is a test email sent from Heroku'
+
 try:
     server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server_ssl.ehlo()   # optional
-    # ...send emails
-    print('Success...')
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sent_from, to, email_text)
+    server.close()
+
+    print ('Email sent!')
 except:
     print('Something went wrong...')
