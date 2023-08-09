9b20a3bd784f1946eb307653a54a1d7dc2933c03
Shane Stevens <45518527+shanestevenz@users.noreply.github.com>
2023-08-09 16:16:00 -0400

    Update test.py

--- test.py
+++ test.py
@@ -11,17 +11,18 @@ body = 'This is a test email sent from Heroku'
 try:
     server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server_ssl.ehlo()   # optional
+    try:
+        server.login(gmail_user, gmail_password)
+        print ('Logged In to Gmail')
+        try:
+            server.sendmail(sent_from, to, email_text)
+            print ('Email sent!')
+        except:
+            print('Could not send mail')
+            server.close()            
+    except:
+        print('Could not login into to gmail')    
 except:
     print('Could not connect to smtp.gmail.com')
-try:
-    server.login(gmail_user, gmail_password)
-    print ('Logged In to Gmail')
-except:
-    print('Could not login into to gmail')
-try:
-    server.sendmail(sent_from, to, email_text)
-    print ('Email sent!')
-except:
-    print('Could not send mail')
-    server.close()
+
 
